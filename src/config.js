8f70a2bae8ecd9ccc3fafe69505149a77bb45a4b
Russ yev <ruslanyev@yahoo.com>
2019-12-03 21:23:43 -0800

    added all known env variables

--- src/config.js
+++ src/config.js
@@ -1,4 +1,16 @@
+"use strict",
+
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || "development"
+  NODE_ENV: process.env.NODE_ENV || "development", 
+  DATABASE_URL: process.env.DATABASE_URL,
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "7d",
+  GOOGLEMAPS_API: process.env.GOOGLEMAPS_API,
+  ZOHO_USER: process.env.ZOHO_USER,
+  ZOHO_PASS: process.env.ZOHO_PASS,
+  ALERT_EMAIL: process.env.ALERT_EMAIL,
+  SUPPORT_EMAIL: process.env.SUPPORT_EMAIL,
+  NOTIFICATION_EMAIL: process.env.NOTIFICATION_EMAIL
 };
