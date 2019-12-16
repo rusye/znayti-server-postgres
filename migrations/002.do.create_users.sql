6d3617c6a5e3171429780e27b687a5233156e5f9
Russ yev <ruslanyev@yahoo.com>
2019-12-15 16:26:36 -0800

    added address_id column to review table and added constraints and defaults to review ratings

--- migrations/002.do.create_users.sql
+++ migrations/002.do.create_users.sql
@@ -9,23 +9,21 @@ CREATE TABLE site_user (
   deleted_on TIMESTAMPTZ DEFAULT now()
 );
 
--- Add review count column to business table
-ALTER TABLE business
-  ADD COLUMN
-    review_count INTEGER NOT NULL;
-
 -- Create a review table to requires the business and user id
 CREATE TABLE review (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id UUID REFERENCES site_user(id) NOT NULL,
   business_id INTEGER REFERENCES business(id) NOT NULL,
-  content TEXT NOT NULL,
-  rating_value INTEGER NOT NULL,
+  address_id INTEGER REFERENCES address(id) NOT NULL,
+  content VARCHAR(5000) NOT NULL,
+  rating_value SMALLINT NOT NULL CHECK(rating_value > 1 AND rating_value < 5),
   review_date TIMESTAMPTZ DEFAULT now() NOT NULL,
   deleted_on TIMESTAMPTZ DEFAULT now()
 );
 
--- Add average rating column to business table
+-- Add average rating and review count columnS to business table
 ALTER TABLE business
   ADD COLUMN
-    average_rating NUMERIC(2, 1) NOT NULL;
+    average_rating NUMERIC(2, 1) NOT NULL DEFAULT 0 CHECK(average_rating > 1.0 AND average_rating < 5.0),
+  ADD COLUMN
+    review_count INTEGER NOT NULL DEFAULT 0;
