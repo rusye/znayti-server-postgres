f0121133a32601cc4dcfc5f330aada32b10806e1
Russ yev <ruslanyev@yahoo.com>
2019-12-16 21:15:47 -0800

    removed not null fields for reviews and ratings in business table and corrected check constraint

--- migrations/002.do.create_users.sql
+++ migrations/002.do.create_users.sql
@@ -16,7 +16,7 @@ CREATE TABLE review (
   business_id INTEGER REFERENCES business(id) NOT NULL,
   address_id INTEGER REFERENCES address(id) NOT NULL,
   content VARCHAR(5000) NOT NULL,
-  rating_value SMALLINT NOT NULL CHECK(rating_value > 1 AND rating_value < 5),
+  rating_value SMALLINT NOT NULL CHECK(rating_value >= 1.0 AND rating_value <= 5.0),
   review_date TIMESTAMPTZ DEFAULT now() NOT NULL,
   deleted_on TIMESTAMPTZ DEFAULT now()
 );
@@ -24,6 +24,6 @@ CREATE TABLE review (
 -- Add average rating and review count columnS to business table
 ALTER TABLE business
   ADD COLUMN
-    average_rating NUMERIC(2, 1) NOT NULL DEFAULT 0 CHECK(average_rating > 1.0 AND average_rating < 5.0),
+    average_rating NUMERIC(2, 1) CHECK(average_rating >= 1.0 AND average_rating <= 5.0),
   ADD COLUMN
-    review_count INTEGER NOT NULL DEFAULT 0;
+    review_count INTEGER DEFAULT 0;
