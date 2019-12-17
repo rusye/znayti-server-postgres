afc4f111703908242140a2bd2e2259488fc832c0
Russ yev <ruslanyev@yahoo.com>
2019-12-16 21:18:36 -0800

    created a test to retrieve inserted test data and compare it to expected results

--- test/businesses-endpoints.spec.js
+++ test/businesses-endpoints.spec.js
@@ -1,6 +1,6 @@
-const { expect } = require("chai");
 const knex = require("knex");
 const app = require("../src/app");
+const testInserts = require("./znayti-fixtures");
 
 describe("Businesses Endpoints", () => {
   let db;
@@ -30,6 +30,38 @@ describe("Businesses Endpoints", () => {
           .get("/api/businesses")
           .set("Authorization", `Bearer ${process.env.API_TOKEN}`)
           .expect(200, []);
+      });
+    });
+
+    context("Given there are businesses in the database", () => {
+      beforeEach("insert categories, addresses, hours, and businesses", () => {
+        return db
+          .into("category")
+          .insert(testInserts.testCategories())
+          .then(() => {
+            return db.into("address").insert(testInserts.testAddresses());
+          })
+          .then(() => {
+            return db.into("business").insert(testInserts.testBusinesses());
+          });
+      });
+
+      const testBusinessesSerilize = businesses => ({
+        ...businesses,
+        review_count: 0,
+        average_rating: null,
+        deleted_on: null
+      });
+
+      const expectedBusinessesResult = testInserts
+        .testBusinesses()
+        .map(testBusinessesSerilize);
+
+      it("Gets businesses from the database", () => {
+        return supertest(app)
+          .get("/api/businesses")
+          .set("Authorization", `Bearer ${process.env.API_TOKEN}`)
+          .expect(200, expectedBusinessesResult);
       });
     });
   });
