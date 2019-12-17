c95d8a581672b131370318c2790fb2466406576e
Russ yev <ruslanyev@yahoo.com>
2019-12-16 18:41:53 -0800

    hours are going to be multiple rows, no longer requiring in business table

--- migrations/001.do.create_znayti_db.sql
+++ migrations/001.do.create_znayti_db.sql
@@ -75,6 +75,19 @@ CREATE TABLE address (
   latitude DOUBLE PRECISION NOT NULL
 );
 
+-- Create the business
+CREATE TABLE business (
+  id SERIAL PRIMARY KEY NOT NULL,
+  visual_id VARCHAR(100) NOT NULL UNIQUE,
+  business_name VARCHAR(100) NOT NULL,
+  contact_name VARCHAR(100) NOT NULL,
+  category_id INTEGER REFERENCES category(id) ON DELETE CASCADE NOT NULL,
+  address_id INTEGER REFERENCES address(id) NOT NULL,
+  google_place VARCHAR(100) NOT NULL,
+  telephone VARCHAR(10) NOT NULL,
+  deleted_on TIMESTAMPTZ DEFAULT now()
+);
+
 -- Created enum for the days of the week
 CREATE TYPE days_of_week AS ENUM (
   'Sunday', 
@@ -88,22 +101,8 @@ CREATE TYPE days_of_week AS ENUM (
 
 -- Created hours table, pk is the business id
 CREATE TABLE hours (
-  id SERIAL PRIMARY KEY NOT NULL,
+  business_id INTEGER REFERENCES business(id) PRIMARY KEY NOT NULL,
   day_of_week days_of_week NOT NULL,
   opens TIME NOT NULL,
   closes TIME NOT NULL
-);
-
--- Create the business
-CREATE TABLE business (
-  id SERIAL PRIMARY KEY NOT NULL,
-  visual_id VARCHAR(100) NOT NULL UNIQUE,
-  business_name VARCHAR(100) NOT NULL,
-  contact_name VARCHAR(100) NOT NULL,
-  category_id INTEGER REFERENCES category(id) ON DELETE CASCADE NOT NULL,
-  address_id INTEGER REFERENCES address(id) NOT NULL,
-  hours_id INTEGER REFERENCES hours(id) NOT NULL,
-  google_place VARCHAR(100) NOT NULL,
-  telephone VARCHAR(10) NOT NULL,
-  deleted_on TIMESTAMPTZ DEFAULT now()
 );
