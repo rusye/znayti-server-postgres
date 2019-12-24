1ca2c734182605f9a8938985d92f0a42856dabc7
Russ yev <ruslanyev@yahoo.com>
2019-12-23 18:26:56 -0800

    added a test to check for API token to use app

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,7 +1,14 @@
 const app = require("../src/app");
 
 describe("App", () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+  it("Responds with 401 Unauthorized for GET /", () => {
+    return supertest(app)
+      .get("/")
+      .set("Authorization", `Bearer someFakeString`)
+      .expect(401, { error: "Unauthorized request" });
+  });
+
+  it('Responds with 200 containing "Hello, world!" for GET /', () => {
     return supertest(app)
       .get("/")
       .set("Authorization", `Bearer ${process.env.API_TOKEN}`)
