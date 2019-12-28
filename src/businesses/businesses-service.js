fabe631f162de7857cab7c5e9c6490dfb7f0040a
Russ yev <ruslanyev@yahoo.com>
2019-12-27 20:45:04 -0800

    made the base query string a function, enforcing DRY

--- src/businesses/businesses-service.js
+++ src/businesses/businesses-service.js
@@ -1,25 +1,28 @@
+const queryString = knex => {
+  return knex
+    .select(
+      "address.*",
+      "address.id as a_id",
+      "category.*",
+      "category.id as c_id",
+      "business.*",
+      knex.raw("array_agg(row_to_json(hours)) as hours")
+    )
+    .from("business")
+    .join("address", "business.address_id", "=", "address.id")
+    .join("category", "business.category_id", "=", "category.id")
+    .leftJoin("hours", "business.id", "=", "hours.id")
+    .groupBy("business.id", "address.id", "category.id")
+    .orderBy("business.id");
+};
+
 const BusinessesService = {
   getAllBusinesses(knex, lat, long, rad) {
-    return knex
-      .select(
-        "address.*",
-        "address.id as a_id",
-        "category.*",
-        "category.id as c_id",
-        "business.*",
-        knex.raw("array_agg(row_to_json(hours)) as hours")
+    return queryString(knex).where(
+      knex.raw(
+        `earth_distance(ll_to_earth(${lat}, ${long}), ll_to_earth(address.latitude, address.longitude)) < ${rad} * 1609.344`
       )
-      .from("business")
-      .join("address", "business.address_id", "=", "address.id")
-      .join("category", "business.category_id", "=", "category.id")
-      .leftJoin("hours", "business.id", "=", "hours.id")
-      .where(
-        knex.raw(
-          `earth_distance(ll_to_earth(${lat}, ${long}), ll_to_earth(address.latitude, address.longitude)) < ${rad} * 1609.344`
-        )
-      )
-      .groupBy("business.id", "address.id", "category.id")
-      .orderBy("business.id");
+    );
   }
 };
 
