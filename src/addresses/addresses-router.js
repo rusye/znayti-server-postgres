b7451b7d26403d4c6878b87275d7ede62e61251f
Russ yev <ruslanyev@yahoo.com>
2020-01-27 22:27:20 -0800

    passing test for POST for required fields

--- src/addresses/addresses-router.js
+++ src/addresses/addresses-router.js
@@ -2,6 +2,8 @@
 
 const express = require("express");
 const xss = require("xss");
+const bodyParser = express.json();
+const logger = require("../logger");
 
 const AddressesService = require("./addresses-service");
 
@@ -13,7 +15,10 @@ const serializeAddress = address => ({
   ...(address.city ? { city: xss(address.city) } : null)
 });
 
-addressesRouter.route("/").get((req, res, next) => {
+addressesRouter
+  .route("/")
+
+  .get((req, res, next) => {
   for (const param of ["zipcode", "city", "street"]) {
     if (!req.query[param]) {
     return res
@@ -35,6 +40,44 @@ addressesRouter.route("/").get((req, res, next) => {
       res.json(addresses.map(serializeAddress));
     })
     .catch(next);
+  })
+
+  .post(bodyParser, (req, res, next) => {
+    const {
+      street,
+      suite,
+      city,
+      state,
+      zipcode,
+      longitude,
+      latitude
+    } = req.body;
+
+    const newAddress = {
+      street,
+      suite,
+      city,
+      state,
+      zipcode,
+      longitude,
+      latitude
+    };
+
+    for (const field of [
+      "street",
+      "city",
+      "state",
+      "zipcode",
+      "longitude",
+      "latitude"
+    ]) {
+      if (!newAddress[field]) {
+        logger.error(`${field} is required`);
+        return res.status(400).send({
+          error: { message: `'${field}' is required` }
+        });
+      }
+    }
 });
 
 module.exports = addressesRouter;
