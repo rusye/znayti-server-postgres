752b2fac38c63b8eb5ac879d99e56d7b49c3525d
Russ yev <ruslanyev@yahoo.com>
2020-01-29 20:07:55 -0800

    passing POST tests for latitude and longitude

--- src/addresses/addresses-router.js
+++ src/addresses/addresses-router.js
@@ -127,6 +127,24 @@ addressesRouter
         }
       });
     }
+
+    if (longitude < -180 || longitude > 180) {
+      logger.error("'longitude' is out of range");
+      return res.status(400).send({
+        error: {
+          message: "'longitude' is out of range, must be between -180 and 180"
+        }
+      });
+    }
+
+    if (latitude < -90 || latitude > 90) {
+      logger.error("'latitude' is out of range");
+      return res.status(400).send({
+        error: {
+          message: "'latitude' is out of range, must be between -90 and 90"
+        }
+      });
+    }
   });
 
 module.exports = addressesRouter;
